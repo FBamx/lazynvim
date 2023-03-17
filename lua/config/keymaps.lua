4c5206819633791376504bcce7f23ebf37223697
elisii <495745954@qq.com>
2023-03-17 22:18:03 +0800

    initialize

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,49 @@
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+-- unset highlight
+map("n", "<Esc>", ":nohl<CR>")
+
+-- local history
+map("n", "<A-h>", ":LocalHistoryToggle<CR>")
+
+-- escape the insert mode
+map("i", "jj", "<Esc>")
+
+-- navigate within insert mode
+map("i", "<C-h>", "<Left>")
+map("i", "<C-l>", "<Right>")
+map("i", "<C-j>", "<Down>")
+map("i", "<C-k>", "<Up>")
+
+map("n", "x", '"_x')
+map("n", "=", "<C-a>")
+map("n", "-", "<C-x>")
+
+map("n", "<C-a>", "ggVG", { silent = true })
+
+map("i", "<M-z>", "<ESC>", { silent = true })
+
+map("n", "<leader>nh", ":nohl<CR>", { silent = true })
+
+-- dap
+map("n", "<F1>", ":lua require 'dap'.toggle_breakpoint()<CR>")
+map("n", "<F2>", ":lua require 'dap'.continue()<CR>")
+map("n", "<F3>", ":lua require 'dap'.step_over()<CR>")
+map("n", "<F4>", ":lua require 'dap'.step_into()<CR>")
+map("n", "<F5>", ":lua require 'dap'.step_out()<CR>")
+map("n", "<F6>", ":lua require 'dapui'.close()<CR>")
+
+-- git
+map("n", "<leader>rh", ":lua require 'gitsigns'.reset_hunk()<CR>")
+map("n", "<leader>ph", ":lua require 'gitsigns'.preview_hunk()<CR>")
+map("n", "<leader>gb", ":lua package.loaded.gitsigns.blame_line()<CR>")
+map("n", "<leader>td", ":lua require 'gitsigns'.toggle_deleted()<CR>")
+
+-- tagbar
+map("n", "<A-t>", ":Tagbar<CR>")
+
+-- commment
+map("n", "<C-m>", ":lua require('Comment.api').toggle.linewise.current()<CR>")
+map("v", "<C-m>", ":lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,58 @@
+return {
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      term_colors = true,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+  -- oxocarbon
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    name = "oxocarbon"
+  },
+  -- gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    opts = {
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = false,
+      strikethrough = false,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = false,
+      overrides = {
+        GruvboxRedSign = { bg = "none" },
+        GruvboxGreenSign = { bg = "none" },
+        GruvboxYellowSign = { bg = "none" },
+        GruvboxBlueSign = { bg = "none" },
+        GruvboxPurpleSign = { bg = "none" },
+        GruvboxAquaSign = { bg = "none" },
+        GruvboxOrangeSign = { bg = "none" }
+      },
+      contrast = "soft",
+      dim_inactive = false,
+      transparent_mode = false,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
