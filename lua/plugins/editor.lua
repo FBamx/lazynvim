4d76389b73fc5fe134bc605d8c2f7061f3b95f1a
elisii <495745954@qq.com>
2023-03-20 02:58:21 +0800

    add plugins go nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -13,6 +13,20 @@ return {
     },
   },
 
+  -- go nvim
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+
   -- dapui
   {
     "rcarriga/nvim-dap-ui",
